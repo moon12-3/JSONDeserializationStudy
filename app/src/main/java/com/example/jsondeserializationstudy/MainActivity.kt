6ec6feb4c2db7b6404cc21896c3810a2aa35183f
pickLotto <s2123@e-mirim.hs.kr>
2022-07-27 11:46:26 +0900

    StdDeserializer을 상속받아 역직렬화

--- app/src/main/java/com/example/jsondeserializationstudy/MainActivity.kt
+++ app/src/main/java/com/example/jsondeserializationstudy/MainActivity.kt
@@ -3,8 +3,50 @@ package com.example.jsondeserializationstudy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+
+@JsonDeserialize(using=ComplexJSONDataDeserializer::class)
+data class ComplexJSONData(
+    val innerData : String,
+    val data1 : Int,
+    val data2: String,
+    val list: List<Int>
+)
+
+class ComplexJSONDataDeserializer : StdDeserializer<ComplexJSONData>(
+    ComplexJSONData::class.java
+) {
+    override fun deserialize(p: JsonParser?,
+                             ctxt: DeserializationContext?): ComplexJSONData {
+        val node = p?.codec?.readTree<JsonNode>(p)
+
+        val nestedNode = node?.get("nested")
+        val innerDataValue = nestedNode?.get("inner_data")?.asText()
+
+        val innerNestedNode = nestedNode?.get("inner_nested")
+        val innerNestedData1 = innerNestedNode?.get("data1")?.asInt()
+        val innerNestedData2 = innerNestedNode?.get("data2")?.asText()
+
+        val list = mutableListOf<Int>()
+        innerNestedNode?.get("list")?.elements()?.forEach {
+            list.add(it.asInt())
+        }
+
+        return ComplexJSONData(
+            innerDataValue!!,
+            innerNestedData1!!,
+            innerNestedData2!!,
+            list
+        )
+    }
+
+}
 
 data class MyJSONDataClass(
     val data1: Int,
@@ -74,6 +116,22 @@ class MainActivity : AppCompatActivity() {
 
         val d3 = mapper?.readValue<Person>(personString)
         Log.d("my_tag", d3.toString())
+
+        val complexJsonString = """
+        {
+            "nested" : {
+                "inner_data" : "Hello from inner",
+                "inner_nested" : {
+                    "data1" : 1234,
+                    "data2" : "Hello",
+                    "list" : [1, 2, 3]
+                }
+            }
+        }
+        """.trimIndent()
+
+        val d4 = mapper?.readValue<ComplexJSONData>(complexJsonString)
+        Log.d("my_tag", d4.toString())
 
     }
 }
